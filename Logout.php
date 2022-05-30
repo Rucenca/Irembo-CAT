c657ae11449dcfc809f218d7051826d8916ce797
Remy Iradukunda <99873842+Rucenca@users.noreply.github.com>
2022-05-30 09:54:28 -0800

    Add files via upload
    Last Commit

---
+++ Logout.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Log Out</title>
+</head>
+<body>
+    <form action="login.php" method="post">
+    <header class="text-white p-3 text-center"style="background-color:blue;">
+<h4>IREMBO | Practical CAT</h4>
+</header>
+
+
+<div class="d-flex justify-content-center p-5 " style="height:80vh;">
+
+<div class="card px-5 pt-4 position-relative text-center w-30">
+          <img src="profile.jpg" class="rounded-circle mx-5" style="width: 90px; height: 90px;"><br>
+           <h5>TUYISHIMIRE Peter</h5><br>
+           <p>tuyishimirepeter@gmail.com</p>
+           <div class="my-5">
+                <a href ="login.php" ><button type="submit" name="logout" class="form-control btn btn-blue fw-bold text-white mt-5" style="background-color:blue;font-size:12px;">
+                    <i class="text-white" aria-hidden="true" style="font-size:5px;"></i>
+                    Logout
+                </button></a>
+           </div>
+        </div>
+</div>
+</form>
+</body>
+</html>
